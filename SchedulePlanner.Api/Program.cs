483c644adda84cca927ce216a7d1154578b57a33
Suppyour <mporsev@mail.ru>
2025-12-05 08:42:35 +0500

    fix

--- SchedulePlanner.Api/Program.cs
+++ SchedulePlanner.Api/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Инфраструктура БД
 builder.Services.AddDbContext<PlannerDbContext>(options =>
     options.UseInMemoryDatabase("PlannerDb"));
 
@@ -16,18 +15,15 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 
-// 2. HTTP Инфраструктура (НОВОЕ)
-// IHttpContextAccessor нужен, чтобы HttpService мог читать текущий TraceId
+
 builder.Services.AddHttpContextAccessor(); 
 
-// Регистрируем типизированный HttpClient для HttpService
 builder.Services.AddHttpClient<IHttpService, HttpService>();
 
-// 3. Application Logic
 builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<CategoryService>();
 builder.Services.AddScoped<TaskService>();
-builder.Services.AddScoped<ExternalIntegrationService>(); // Наш новый сервис
+builder.Services.AddScoped<ExternalIntegrationService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -35,14 +31,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// 4. Подключаем Middleware (ВАЖНО: Порядок имеет значение)
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// TraceIdMiddleware должен быть как можно раньше, чтобы поймать запрос первым
 app.UseMiddleware<TraceIdMiddleware>();
 
 app.MapControllers();
